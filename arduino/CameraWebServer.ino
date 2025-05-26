a208c8bac35303dcd8dd35fc6b77aaa9ba1fd2ef
toranii <140582349+toranii@users.noreply.github.com>
2025-05-26 22:29:56 +0900

    Update CameraWebServer.ino

--- arduino/CameraWebServer.ino
+++ arduino/CameraWebServer.ino
@@ -1,52 +1,84 @@
 #include "esp_camera.h"
 #include <WiFi.h>
+#include <EEPROM.h>
+#include "app_httpd.h"
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
 
-//
-// WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
-//            Ensure ESP32 Wrover Module or other board with PSRAM is selected
-//            Partial images will be transmitted if image exceeds buffer size
-//
-//            You must select partition scheme from the board menu that has at least 3MB APP space.
-//            Face Recognition is DISABLED for ESP32 and ESP32-S2, because it takes up from 15
-//            seconds to process single frame. Face Detection is ENABLED if PSRAM is enabled as well
-
-// ===================
-// Select camera model
-// ===================
-//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
-//#define CAMERA_MODEL_ESP_EYE  // Has PSRAM
-//#define CAMERA_MODEL_ESP32S3_EYE // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
-//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
-//#define CAMERA_MODEL_M5STACK_UNITCAM // No PSRAM
-//#define CAMERA_MODEL_M5STACK_CAMS3_UNIT  // Has PSRAM
-#define CAMERA_MODEL_AI_THINKER // Has PSRAM
-//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
-//#define CAMERA_MODEL_XIAO_ESP32S3 // Has PSRAM
-// ** Espressif Internal Boards **
-//#define CAMERA_MODEL_ESP32_CAM_BOARD
-//#define CAMERA_MODEL_ESP32S2_CAM_BOARD
-//#define CAMERA_MODEL_ESP32S3_CAM_LCD
-//#define CAMERA_MODEL_DFRobot_FireBeetle2_ESP32S3 // Has PSRAM
-//#define CAMERA_MODEL_DFRobot_Romeo_ESP32S3 // Has PSRAM
+#define CAMERA_MODEL_AI_THINKER
 #include "camera_pins.h"
 
-// ===========================
-// Enter your WiFi credentials
-// ===========================
-const char *ssid = "BigDATA-2.4G";
-const char *password = "qlr!epdlxj!!";
+const char* ssid = "BigDATA-2.4G";
+const char* password = "qlr!epdlxj!!";
+const char* serverUrl = "http://192.168.0.11:5000/api/device/register";  // 백엔드 API 주소
 
-void startCameraServer();
-void setupLedFlash(int pin);
+#define EEPROM_SIZE 128
+#define TOKEN_ADDR 0
 
-void setup() {
-  Serial.begin(115200);
-  Serial.setDebugOutput(true);
-  Serial.println();
+String token;
 
+String generateToken() {
+  const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  String newToken = "";
+  for (int i = 0; i < 32; i++) {
+    newToken += charset[random(0, sizeof(charset) - 1)];
+  }
+  return newToken;
+}
+
+void registerDevice(String token) {
+  if ((WiFi.status() == WL_CONNECTED)) {
+    HTTPClient http;
+    http.begin(serverUrl);
+    http.addHeader("Content-Type", "application/json");
+
+    StaticJsonDocument<300> doc;
+    doc["mac"] = WiFi.macAddress();
+    doc["token"] = token;
+    doc["device_name"] = "MyESP32CAM";
+    doc["ip_address"] = WiFi.localIP().toString();
+
+    String jsonPayload;
+    serializeJson(doc, jsonPayload);
+
+    Serial.println("[등록 요청 전송]");
+    Serial.println(jsonPayload);
+
+    int httpResponseCode = http.POST(jsonPayload);
+    Serial.printf("[서버 응답 코드] %d\n", httpResponseCode);
+    String response = http.getString();
+    Serial.println("[서버 응답 본문]");
+    Serial.println(response);
+
+    http.end();
+  } else {
+    Serial.println("[오류] WiFi가 연결되지 않아 장치 등록 실패");
+  }
+}
+
+void saveTokenToEEPROM(String tokenToSave) {
+  EEPROM.begin(EEPROM_SIZE);
+  for (int i = 0; i < tokenToSave.length(); i++) {
+    EEPROM.write(TOKEN_ADDR + i, tokenToSave[i]);
+  }
+  EEPROM.write(TOKEN_ADDR + tokenToSave.length(), '\0');
+  EEPROM.commit();
+  EEPROM.end();
+}
+
+String readTokenFromEEPROM() {
+  EEPROM.begin(EEPROM_SIZE);
+  String loadedToken = "";
+  for (int i = 0; i < 64; i++) {
+    char c = EEPROM.read(TOKEN_ADDR + i);
+    if (c == '\0') break;
+    loadedToken += c;
+  }
+  EEPROM.end();
+  return loadedToken;
+}
+
+void initCamera() {
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
@@ -67,97 +99,51 @@ void setup() {
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.frame_size = FRAMESIZE_UXGA;
-  config.pixel_format = PIXFORMAT_JPEG;  // for streaming
-  //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
-  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
-  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_SVGA;
   config.jpeg_quality = 12;
-  config.fb_count = 1;
+  config.fb_count = 2;
 
-  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
-  //                      for larger pre-allocated frame buffer.
-  if (config.pixel_format == PIXFORMAT_JPEG) {
-    if (psramFound()) {
-      config.jpeg_quality = 10;
-      config.fb_count = 2;
-      config.grab_mode = CAMERA_GRAB_LATEST;
-    } else {
-      // Limit the frame size when PSRAM is not available
-      config.frame_size = FRAMESIZE_SVGA;
-      config.fb_location = CAMERA_FB_IN_DRAM;
-    }
-  } else {
-    // Best option for face detection/recognition
-    config.frame_size = FRAMESIZE_240X240;
-#if CONFIG_IDF_TARGET_ESP32S3
-    config.fb_count = 2;
-#endif
-  }
-
-#if defined(CAMERA_MODEL_ESP_EYE)
-  pinMode(13, INPUT_PULLUP);
-  pinMode(14, INPUT_PULLUP);
-#endif
-
-  // camera init
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
+    Serial.printf("[카메라 초기화 실패] 오류코드: 0x%x\n", err);
     return;
   }
+}
 
-  sensor_t *s = esp_camera_sensor_get();
-  // initial sensors are flipped vertically and colors are a bit saturated
-  if (s->id.PID == OV3660_PID) {
-    s->set_vflip(s, 1);        // flip it back
-    s->set_brightness(s, 1);   // up the brightness just a bit
-    s->set_saturation(s, -2);  // lower the saturation
-  }
-  // drop down frame size for higher initial frame rate
-  if (config.pixel_format == PIXFORMAT_JPEG) {
-    s->set_framesize(s, FRAMESIZE_QVGA);
-  }
-
-#if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
-  s->set_vflip(s, 1);
-  s->set_hmirror(s, 1);
-#endif
-
-#if defined(CAMERA_MODEL_ESP32S3_EYE)
-  s->set_vflip(s, 1);
-#endif
-
-// Setup LED FLash if LED pin is defined in camera_pins.h
-#if defined(LED_GPIO_NUM)
-  setupLedFlash(LED_GPIO_NUM);
-#endif
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
 
   WiFi.begin(ssid, password);
-  WiFi.setSleep(false);
-
   Serial.print("WiFi connecting");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
+  delay(2000);
+  Serial.println("\nWiFi connected!");
+  Serial.println(WiFi.localIP());
 
-  Serial.print("ESP32 STA MAC Address (WiFi Station): ");
-  Serial.println(WiFi.macAddress());
+  initCamera();
 
-  Serial.print("ESP32 AP MAC Address (Access Point): ");
-  Serial.println(WiFi.softAPmacAddress());
+  token = readTokenFromEEPROM();
+  if (token.length() == 0) {
+    token = generateToken();
+    saveTokenToEEPROM(token);
+    Serial.println("[토큰 생성 및 저장 완료] " + token);
+  } else {
+    Serial.println("[저장된 토큰 사용] " + token);
+  }
+
+  Serial.println("[정보] ESP32는 토큰 발급기 역할만 수행합니다. 등록은 웹에서 처리됩니다.");
+
+  registerDevice(token);
 
   startCameraServer();
-
-  Serial.print("Camera Ready! Use 'http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("' to connect");
+  Serial.println("Camera Ready! Use '/stream' to view");
 }
 
 void loop() {
-  // Do nothing. Everything is done in another task by the web server
   delay(10000);
 }
